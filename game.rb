4591a723dc235f81b1907190544c3c76408f625d
Hatem <hatem.deux@gmail.com>
2020-04-28 12:42:31 -0400

    minor bugs fixed

--- game.rb
+++ game.rb
@@ -6,38 +6,44 @@ class Game
   def initialize
     @player1 = Player.new("Player1", "P1")
     @player2 = Player.new("Player2", "P2")
-    @turn_number = 1
+    # @turn_number = 1
 
   end
 
 
   def next_turn
     puts "----- NEW TURN -----"
-    @turn_number += 1    
+    # @turn_number += 1
   end
 
 
   def display_turn_status
     # puts "P1: #/3 vs. P2: #/3"
-    puts "-- #{@player1.abbrv}: #{@player1.lives}/#{@player1.full_lives} \
-    vs.#{@player2.abbrv}: #{@player2.lives}/#{@player2.full_lives} -- "
+    puts "-- #{@player1.abbrv}: #{@player1.lives}/#{@player1.full_lives}"\
+    " vs. #{@player2.abbrv}: #{@player2.lives}/#{@player2.full_lives} -- "
   end
 
 
   def game_over?
-    @player1.dead?|| @player2.dead?
+    @player1.dead? || @player2.dead?
   end
 
 
   def display_winner()
-    if (@player1.dead?) 
+    if (@player1.dead? && @player2.dead?)
+      puts "It's a tie! All is lost! All is dead!"
+      # return
+    else 
+      if(@player1.dead?) 
       looser = @player1
       winner = @player2
-    else 
+      else 
       looser = @player2
       winner = @player1
+      end
+      puts "#{winner.name} wins with a score of #{winner.wins}/#{looser.wins}" #
     end 
-    puts "#{winner.name} wins with a score of #{winner.wins}/#{looser.wins}" #
+
     puts "----- GAME OVER -----"
     puts "Good bye !"
   end
@@ -60,10 +66,12 @@ class Game
       question2.correct? ? @player2.wins += 1 : @player2.takes_life
       
       puts
+
       display_turn_status()
       
       puts
-      next_turn()
+
+      !game_over? && next_turn()
             
     end
     puts 
